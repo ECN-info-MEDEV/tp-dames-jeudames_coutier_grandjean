31902913a5d31553c705ca243cdbe5aaf2466559
ManonC12 <manon-c.coutier@wanadoo.fr>
2022-12-15 11:30:12 +0100

    create test pion

---
+++ src/test/java/org/centrale/jeudedames/PionTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package org.centrale.jeudedames;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Manon Coutier
+ */
+public class PionTest {
+    
+    public PionTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+
+    
+
+    /**
+     * Test of promotion method, of class Pion.
+     */
+    @Test
+    public void testPromotion() {
+        System.out.println("promotion");
+        Pion instance = new Pion();
+        instance.promotion();
+    }
+    
+}
